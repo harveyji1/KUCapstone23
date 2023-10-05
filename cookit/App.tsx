33e497c18c54c1afe47c3500211954b8852c0ea2
pinoaudrey <pinoaudreyl@gmail.com>
2023-10-04 20:08:18 -0500

    Add Fonts

--- cookit/App.tsx
+++ cookit/App.tsx
@@ -4,11 +4,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DetailsScreen } from './src/screens/Profile';
-import { LoginPage } from './src/screens/Login/Login'
+import { LoginPage } from './src/screens/Login';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 function HomeScreen({navigation}){
   return (
-    <View style={styles.container}>
+    <View style={styles.container} >
       <Text style={styles.text}>Cookit Home Screen</Text>
       <Button
         title="Go to Details"
@@ -42,30 +47,76 @@ function HomeScreen({navigation}){
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    'YoungSerif-Regular': require('./assets/fonts/YoungSerif-Regular.ttf'),
+
+    'SweetSansProRegular': require('./assets/fonts/SweetSansProRegular.otf'),
+    'SweetSansProBold': require('./assets/fonts/SweetSansProBold.otf'),
+    'SweetSansProLight': require('./assets/fonts/SweetSansProLight.otf'),
+    'SweetSansProMedium': require('./assets/fonts/SweetSansProMedium.otf'),
+
+    'Trispace-Bold': require('./assets/fonts/Trispace-Bold.ttf'),
+    'Trispace-Regular': require('./assets/fonts/Trispace-Regular.ttf'),
+    'Trispace-Light': require('./assets/fonts/Trispace-Light.ttf'),
+    'Trispace-Medium': require('./assets/fonts/Trispace-Medium.ttf'),
+    'Trispace-ExtraBold': require('./assets/fonts/Trispace-ExtraBold.ttf'),
+    'Trispace-SemiBold': require('./assets/fonts/Trispace-SemiBold.ttf'),
+
+    'PlayfairDisplay-Black': require('./assets/fonts/PlayfairDisplay-Black.ttf'),
+    'PlayfairDisplay-Bold': require('./assets/fonts/PlayfairDisplay-Bold.ttf'),
+    'PlayfairDisplay-ExtraBold': require('./assets/fonts/PlayfairDisplay-ExtraBold.ttf'),
+    'PlayfairDisplay-Medium': require('./assets/fonts/PlayfairDisplay-Medium.ttf'),
+    'PlayfairDisplay-Regular': require('./assets/fonts/PlayfairDisplay-Regular.ttf'),
+    'PlayfairDisplay-SemiBold': require('./assets/fonts/PlayfairDisplay-SemiBold.ttf'),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync(); //hide the splashscreen
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>{
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen
+  <><View style={styles.font} onLayout={handleOnLayout}></View>
+  <NavigationContainer>{<Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen
         name="Login"
-        component={LoginPage}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Overview' }}
-        />
-        <Stack.Screen 
-          name="Details" 
-          component={DetailsScreen}
-          initialParams={{itemID: 0, otherParam: 'nothing'}}
-          />
-      </Stack.Navigator>
-      
-    }</NavigationContainer>
+        component={LoginPage} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Overview' }} />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        initialParams={{ itemID: 0, otherParam: 'nothing' }} />
+    </Stack.Navigator>}</NavigationContainer></>
     
   );
 }
-
+{/* <NavigationContainer>{
+  <Stack.Navigator initialRouteName='Login'>
+    <Stack.Screen
+    name="Login"
+    component={LoginPage}
+    />
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{ title: 'Overview' }}
+    />
+    <Stack.Screen 
+      name="Details" 
+      component={DetailsScreen}
+      initialParams={{itemID: 0, otherParam: 'nothing'}}
+      />
+  </Stack.Navigator>
+  
+}</NavigationContainer> */}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -76,6 +127,9 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#437c90'
+  },
+  font: {
+    // fontFamily: 'SweetSansProRegular',
   }
 });
 
