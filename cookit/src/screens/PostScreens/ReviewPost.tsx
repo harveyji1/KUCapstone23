d5b346dfb4b150f24f49fdaf9729d3f6d6164171
czajkatony <4tonyc4@gmail.com>
2024-02-26 16:24:12 -0600

    Halfway progress, need to display on screen

--- cookit/src/screens/PostScreens/ReviewPost.tsx
+++ cookit/src/screens/PostScreens/ReviewPost.tsx
@@ -24,16 +24,12 @@ export function ReviewPostScreen({ navigation, route }) {
 
   const HandlePost = async () => {
     // convert list to string
+    let priceAsString = parseFloat(estimatedPrice).toFixed(2);
     let ingredientsString = ingredientsList.join(", "); // Join the array elements with a comma and a space
     // convert image to binary
 
     const url = `http://localhost:${LOCAL_HOST_NUBMER}/api/v1.0/posts`;
-    const formData = new FormData();
-    formData.append("Title", recipeName);
-    formData.append("Ingredients", ingredientsString);
-    formData.append("Instructions", instructions);
-    formData.append("Cost", estimatedPrice);
-    formData.append("PrepTime", combinedCookTime);
+
     // need to append bob for image later
 
     // const data = {
@@ -53,16 +49,31 @@ export function ReviewPostScreen({ navigation, route }) {
       // API call
       const response1 = await fetch(image);
       const blob = await response1.blob();
-      formData.append("PostImage", blob, blob._data.name);
+      const formData = new FormData();
+      formData.append("Title", recipeName);
+      formData.append("Ingredients", ingredientsString);
+      formData.append("Instructions", instructions);
+      formData.append("Cost", priceAsString);
+      formData.append("PrepTime", combinedCookTime);
+      formData.append("PostImage", blob, "image.jpg");
 
       // Debug stuff
       console.log("Recipe Name: ", recipeName);
       console.log("Description: ", description);
       console.log("cookingTime: ", combinedCookTime);
       console.log("cost: ", estimatedPrice);
-      console.log("imageBlob: ", blob._data.name);
+      console.log("imageBlob: ", blob);
       console.log("ingredients list: ", ingredientsString);
       console.log("INstructions: ", instructions);
+
+      // // Log each key-value pair
+      // for (let [key, value] of formData.entries()) {
+      //   if ((key = "PostImage")) {
+      //     console.log("BLOB HERE");
+      //   } else {
+      //     console.log(`${key}: ${value}`);
+      //   }
+      // }
 
       const response = await axios.post(url, formData, config);
       console.log("Response: ", response.data);
@@ -70,8 +81,21 @@ export function ReviewPostScreen({ navigation, route }) {
       // Navigate back to create post
       navigation.navigate("CreatePost");
     } catch (error) {
-      // Error handle api call
-      console.error("Error: ", error);
+      // Handle error
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error("Error Data:", error.response.data);
+        console.error("Error Status:", error.response.status);
+        console.error("Error Headers:", error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("Error Request:", error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error Message:", error.message);
+      }
+      console.error("Error Config:", error.config);
     }
   };
   return (
