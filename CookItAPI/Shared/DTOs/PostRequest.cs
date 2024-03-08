76abd4dd392791a0ecb545ddeddd138075a788f4
Harvey Ji <harvey.ji123@gmail.com>
2024-03-08 16:27:01 -0600

    Some weird stuff happened and things got deleted somehow?

---
+++ CookItAPI/Shared/DTOs/PostRequest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    /// <summary>
+    /// post request object available to all layers
+    /// </summary>
+    public class PostRequest
+    {
+        public string Title { get; set; }
+        public string? Ingredients { get; set; }
+        public string? Instructions { get; set; }
+        public decimal Cost { get; set; }
+        public string? PrepTime { get; set; }
+        public IFormFile PostImage { get; set; }
+    }
+}
---
+++ CookItAPI/Shared/DTOs/ProfileRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Shared.DTOs
+{
+    /// <summary>
+    /// request object available to all layers
+    /// </summary>
+    public class ProfileRequest
+    {
+        public string? FullName { get; set; }
+        public string? Bio { get; set; }
+    }
+}
