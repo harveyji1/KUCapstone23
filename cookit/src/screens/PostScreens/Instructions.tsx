060ed584ef35197af3da4704e0b7220fd7c0d488
czajkatony <4tonyc4@gmail.com>
2024-03-29 15:26:44 -0500

    Remove useless review post screen

--- cookit/src/screens/PostScreens/Instructions.tsx
+++ cookit/src/screens/PostScreens/Instructions.tsx
@@ -4,7 +4,7 @@
   Editors: Audrey Pino
 */
 
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   View,
   TextInput,
@@ -17,13 +17,19 @@ import {
 } from "react-native";
 import { PlusIcon, TrashIcon, MenuIcon } from "../../../assets/recipe-icons";
 import DragList, { DragListRenderItemInfo } from "react-native-draglist";
+import axios from "axios";
+import { LoginContext } from "../../../LoginProvider";
+import * as FileSystem from "expo-file-system";
 
 interface instructions {
   id: string;
   instruction: string;
 }
+const LOCAL_HOST_NUBMER = "5018";
 
 export function InstructionsScreen({ route, navigation }) {
+  const { state } = React.useContext(LoginContext);
+  let loginToken = state;
   const {
     recipeName,
     // tagsChef,
@@ -124,6 +130,103 @@ export function InstructionsScreen({ route, navigation }) {
     return typeof item === "object" ? item.id.toString() : item;
   };
 
+  const handlePost = async () => {
+    // convert list to string
+    let priceAsString = parseFloat(estimatedPrice).toFixed(2);
+    // convert image to binary
+
+    const url = `http://localhost:${LOCAL_HOST_NUBMER}/api/v1.0/posts`;
+
+    // need to append bob for image later
+
+    // const data = {
+    //   Title: recipeName,
+    //   Ingredients: ingredientsString,
+    //   Cost: estimatedPrice,
+    //   PrepTime: combinedCookTime,
+    //   PostImage: image,
+    // };
+    const config = {
+      headers: {
+        Authorization: `Bearer ${loginToken}`,
+      },
+    };
+
+    try {
+      // API call
+
+      // Format instructions
+      let formattedInstructions = instructions
+        .map((instruction) => instruction.instruction)
+        .join("|");
+
+      // file system stuff
+      // Read the image file as a base64-encoded string
+      const base64 = await FileSystem.readAsStringAsync(image, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+
+      // Create a Blob object from the base64 string
+      // const blob = new Blob([base64], { type: "image/jpeg" }); // Replace 'image/jpeg' with the correct MIME type of your image
+      // const contents = await FileSystem.readAsStringAsync(image);
+
+      const formData = new FormData();
+      formData.append("Title", recipeName);
+      formData.append("Ingredients", ingredientsString);
+      formData.append("Instructions", formattedInstructions);
+      formData.append("Cost", priceAsString);
+      formData.append("PrepTime", combinedCookTime);
+      formData.append("Description", description);
+      // Append the base64 image to the FormData
+      formData.append("PostImage", {
+        uri: `data:image/jpeg;base64,${base64}`,
+        name: "image.jpg",
+        type: "image/jpeg",
+      });
+      // formData.append("PostImage", blob);
+
+      // Debug stuff
+      console.log("Recipe Name: ", recipeName);
+      console.log("Description: ", description);
+      console.log("cookingTime: ", combinedCookTime);
+      console.log("cost: ", estimatedPrice);
+      console.log("imageBlob: ", "Test");
+      console.log("ingredients list: ", ingredientsString);
+      console.log("INstructions: ", instructions);
+
+      // // Log each key-value pair
+      // for (let [key, value] of formData.entries()) {
+      //   if ((key = "PostImage")) {
+      //     console.log("BLOB HERE");
+      //   } else {
+      //     console.log(`${key}: ${value}`);
+      //   }
+      // }
+
+      const response = await axios.post(url, formData, config);
+      console.log("Response: ", response.data);
+
+      // Navigate back to create post
+      navigation.navigate("CreatePost");
+    } catch (error) {
+      // Handle error
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error("Error Data:", error.response.data);
+        console.error("Error Status:", error.response.status);
+        console.error("Error Headers:", error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("Error Request:", error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error Message:", error.message);
+      }
+      console.error("Error Config:", error.config);
+    }
+  };
+
   // Main return statement rendering the UI elements and the button to navigate to the next screen
   return (
     <View style={styles.container}>
@@ -139,26 +242,7 @@ export function InstructionsScreen({ route, navigation }) {
       </View>
       <View style={styles.buttonContainer}>
         <View style={styles.button}>
-          <Button
-            color="#FFF"
-            title="Post"
-            onPress={() =>
-              navigation.navigate("Review", {
-                recipeName,
-                // tagsChef,
-                combinedPrepTime,
-                combinedCookTime,
-                estimatedPrice,
-                description,
-                tagInput,
-                image,
-                ingredientsString,
-                instructions: instructions
-                  .map((instruction) => instruction.instruction)
-                  .join("|"), // Pass the instructions text only
-              })
-            }
-          />
+          <Button color="#FFF" title="Post" onPress={handlePost} />
         </View>
       </View>
     </View>
