4c9e75cc0a2410bb60c64ddd9b9b23c70d6a7e12
pinoaudrey <pinoaudreyl@gmail.com>
2024-03-29 19:52:50 -0500

    header fixes

--- cookit/assets/recipe-icons.tsx
+++ cookit/assets/recipe-icons.tsx
@@ -341,6 +341,7 @@ const PrepTimeIcon = () => (
   export {
     PasswordIcon
   }
+
   const BioIcon = () => (
     <View>
       <Svg viewBox="0 0 20 20" width="20" height="20">
@@ -354,4 +355,18 @@ const PrepTimeIcon = () => (
   
   export {
     BioIcon
+  }
+
+  const FilterIcon = () => (
+    <View>
+      <Svg viewBox="0 0 20 20" width="20" height="20">
+        <Path d="M21 7.75H3C2.59 7.75 2.25 7.41 2.25 7C2.25 6.59 2.59 6.25 3 6.25H21C21.41 6.25 21.75 6.59 21.75 7C21.75 7.41 21.41 7.75 21 7.75Z" fill="#FFFFFF"/>
+        <Path d="M18 12.75H6C5.59 12.75 5.25 12.41 5.25 12C5.25 11.59 5.59 11.25 6 11.25H18C18.41 11.25 18.75 11.59 18.75 12C18.75 12.41 18.41 12.75 18 12.75Z" fill="#FFFFFF"/>
+        <Path d="M14 17.75H10C9.59 17.75 9.25 17.41 9.25 17C9.25 16.59 9.59 16.25 10 16.25H14C14.41 16.25 14.75 16.59 14.75 17C14.75 17.41 14.41 17.75 14 17.75Z" fill="#FFFFFF"/>
+      </Svg>
+    </View>
+  )
+  
+  export {
+    FilterIcon
   }
